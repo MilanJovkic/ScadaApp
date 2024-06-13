79127ac285ec04e33f224369adf9864f118bc7e7
milanovicmilos <104598163+milanovicmilos@users.noreply.github.com>
2024-06-13 14:59:34 +0200

    fix: changed menu

--- ScadaCore/HostScadaCore/Program.cs
+++ ScadaCore/HostScadaCore/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.ServiceModel;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using HostScadaCore.ServiceReference1;
 
 namespace HostScadaCore
@@ -13,96 +8,175 @@ namespace HostScadaCore
     {
         private static readonly ServiceReference2.UserProcessingClient userProcessingClient = new ServiceReference2.UserProcessingClient();
         private static readonly ServiceReference2.TagProcessingClient tagProcessingClient = new ServiceReference2.TagProcessingClient();
+        private static bool isLoggedIn = false;
+        private static string loggedInUser = string.Empty;
+
         static void Main(string[] args)
         {
             Console.WriteLine("SCADA Core Service is running...");
+
             while (true)
             {
-                Console.Clear();
-                Console.WriteLine("SCADA System Menu:");
-                Console.WriteLine("1. Add Tag");
-                Console.WriteLine("2. Remove Tag");
-                Console.WriteLine("3. Set Tag Value");
-                Console.WriteLine("4. Get Tag Value");
-                Console.WriteLine("5. Turn Scan On/Off");
-                Console.WriteLine("6. Register User");
-                Console.WriteLine("7. Login");
-                Console.WriteLine("8. Logout");
-                Console.WriteLine("9. Exit");
-
-                Console.Write("Select an option: ");
-                string option = Console.ReadLine();
-
-                switch (option)
+                if (!isLoggedIn)
                 {
-                    case "1":
-                        Console.Write("Enter tag name: ");
-                        string tagName = Console.ReadLine();
-                        //tagProcessingClient.AddTag(new Tag { Name = tagName });
-                        Console.WriteLine("Tag added.");
-                        break;
-                    case "2":
-                        Console.Write("Enter tag name: ");
-                        tagName = Console.ReadLine();
-                        tagProcessingClient.RemoveTag(tagName);
-                        Console.WriteLine("Tag removed.");
-                        break;
-                    case "3":
-                        Console.Write("Enter tag name: ");
-                        tagName = Console.ReadLine();
-                        Console.Write("Enter tag value: ");
-                        double tagValue = double.Parse(Console.ReadLine());
-                        tagProcessingClient.SetTagValue(tagName, tagValue);
-                        Console.WriteLine("Tag value set.");
-                        break;
-                    case "4":
-                        Console.Write("Enter tag name: ");
-                        tagName = Console.ReadLine();
-                        double value = tagProcessingClient.GetTagValue(tagName);
-                        Console.WriteLine($"Tag value: {value}");
-                        break;
-                    case "5":
-                        Console.Write("Enter tag name: ");
-                        tagName = Console.ReadLine();
-                        Console.Write("Turn scan on (true/false): ");
-                        bool scanOn = bool.Parse(Console.ReadLine());
-                        tagProcessingClient.TurnScanOnOff(tagName, scanOn);
-                        Console.WriteLine("Scan status changed.");
-                        break;
-                    case "6":
-                        Console.Write("Enter username: ");
-                        string username = Console.ReadLine();
-                        Console.Write("Enter password: ");
-                        string password = Console.ReadLine();
-                        userProcessingClient.RegisterUser(username, password);
-                        Console.WriteLine("User registered.");
-                        break;
-                    case "7":
-                        Console.Write("Enter username: ");
-                        username = Console.ReadLine();
-                        Console.Write("Enter password: ");
-                        password = Console.ReadLine();
-                        bool loggedIn = userProcessingClient.Login(username, password);
-                        Console.WriteLine(loggedIn ? "Login successful." : "Login failed.");
-                        break;
-                    case "8":
-                        Console.Write("Enter username: ");
-                        username = Console.ReadLine();
-                        userProcessingClient.Logout(username);
-                        Console.WriteLine("User logged out.");
-                        break;
-                    case "9":
-                        Console.WriteLine("Exiting...");
-                        return;
-                    default:
-                        // close the connection
-                        Console.WriteLine("Invalid option. Please try again.");
-                        break;
+                    Console.Clear();
+                    Console.WriteLine("Welcome to SCADA System");
+                    Console.WriteLine("1. Register User");
+                    Console.WriteLine("2. Login");
+                    Console.WriteLine("3. Exit");
+
+                    Console.Write("Select an option: ");
+                    string option = Console.ReadLine();
+
+                    switch (option)
+                    {
+                        case "1":
+                            RegisterUser();
+                            break;
+                        case "2":
+                            Login();
+                            break;
+                        case "3":
+                            Console.WriteLine("Exiting...");
+                            return;
+                        default:
+                            Console.WriteLine("Invalid option. Please try again.");
+                            break;
+                    }
+                }
+                else
+                {
+                    ShowMainMenu();
                 }
 
                 Console.WriteLine("Press any key to continue...");
                 Console.ReadKey();
             }
+        }
+
+        static void RegisterUser()
+        {
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+            userProcessingClient.RegisterUser(username, password);
+            Console.WriteLine("User registered.");
+        }
+
+        static void Login()
+        {
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+            bool success = userProcessingClient.Login(username, password);
+            if (success)
+            {
+                isLoggedIn = true;
+                loggedInUser = username;
+                Console.WriteLine("Login successful.");
+            }
+            else
+            {
+                Console.WriteLine("Login failed. Please try again.");
+            }
+        }
+
+        static void Logout()
+        {
+            userProcessingClient.Logout(loggedInUser);
+            isLoggedIn = false;
+            loggedInUser = string.Empty;
+            Console.WriteLine("User logged out.");
+        }
+
+        static void ShowMainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("SCADA System Menu:");
+            Console.WriteLine("1. Add Tag");
+            Console.WriteLine("2. Remove Tag");
+            Console.WriteLine("3. Set Tag Value");
+            Console.WriteLine("4. Get Tag Value");
+            Console.WriteLine("5. Turn Scan On/Off");
+            Console.WriteLine("6. Logout");
+            Console.WriteLine("7. Exit");
+
+            Console.Write("Select an option: ");
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    AddTag();
+                    break;
+                case "2":
+                    RemoveTag();
+                    break;
+                case "3":
+                    SetTagValue();
+                    break;
+                case "4":
+                    GetTagValue();
+                    break;
+                case "5":
+                    TurnScanOnOff();
+                    break;
+                case "6":
+                    Logout();
+                    break;
+                case "7":
+                    Console.WriteLine("Exiting...");
+                    return;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+        }
+
+        static void AddTag()
+        {
+            Console.Write("Enter tag name: ");
+            string tagName = Console.ReadLine();
+            //tagProcessingClient.AddTag(new Tag { Name = tagName });
+            Console.WriteLine("Tag added.");
+        }
+
+        static void RemoveTag()
+        {
+            Console.Write("Enter tag name: ");
+            string tagName = Console.ReadLine();
+            tagProcessingClient.RemoveTag(tagName);
+            Console.WriteLine("Tag removed.");
+        }
+
+        static void SetTagValue()
+        {
+            Console.Write("Enter tag name: ");
+            string tagName = Console.ReadLine();
+            Console.Write("Enter tag value: ");
+            double tagValue = double.Parse(Console.ReadLine());
+            tagProcessingClient.SetTagValue(tagName, tagValue);
+            Console.WriteLine("Tag value set.");
+        }
+
+        static void GetTagValue()
+        {
+            Console.Write("Enter tag name: ");
+            string tagName = Console.ReadLine();
+            double value = tagProcessingClient.GetTagValue(tagName);
+            Console.WriteLine($"Tag value: {value}");
+        }
+
+        static void TurnScanOnOff()
+        {
+            Console.Write("Enter tag name: ");
+            string tagName = Console.ReadLine();
+            Console.Write("Turn scan on (true/false): ");
+            bool scanOn = bool.Parse(Console.ReadLine());
+            tagProcessingClient.TurnScanOnOff(tagName, scanOn);
+            Console.WriteLine("Scan status changed.");
         }
     }
 }
